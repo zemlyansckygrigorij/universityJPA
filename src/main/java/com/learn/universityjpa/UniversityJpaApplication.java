7f85b3985fa4b96bda13ded3fe35c051812efe74
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-10-08 16:29:50 +0500

    Initial commit

---
+++ src/main/java/com/learn/universityjpa/UniversityJpaApplication.java
@@ -0,0 +1,13 @@
+package com.learn.universityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityJpaApplication.class, args);
+    }
+
+}
