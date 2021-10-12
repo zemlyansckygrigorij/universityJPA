aaa3236be1aa137e4794104b5eab96aac0a2981b
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-10-12 14:27:55 +0500

    created interface StudentRepository

---
+++ src/main/java/com/learn/universityjpa/repo/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.learn.universityjpa.repo;
+
+import com.learn.universityjpa.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
