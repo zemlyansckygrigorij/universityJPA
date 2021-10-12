b5154e5399fda8caa7efdf932005f676c53e63fb
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-10-12 14:28:13 +0500

    created interface GroupRepository

---
+++ src/main/java/com/learn/universityjpa/repo/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.learn.universityjpa.repo;
+
+import com.learn.universityjpa.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
