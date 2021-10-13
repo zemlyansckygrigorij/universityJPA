b56bf88567e746105073ec1db9f46b3773229325
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-10-13 14:55:52 +0500

    update class Student update @ManyToOne

--- src/main/java/com/learn/universityjpa/entity/Student.java
+++ src/main/java/com/learn/universityjpa/entity/Student.java
@@ -1,13 +1,11 @@
 package com.learn.universityjpa.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "student")
 public class Student  extends Person {
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "id_group", nullable = false)
     private Group group;
 }
