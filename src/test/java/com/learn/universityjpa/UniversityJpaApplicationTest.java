f5081031296c78b4de2ccd91a3abbcd429624283
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-10-13 14:54:44 +0500

    create test class UniversityJpaApplicationTest for testing spring

---
+++ src/test/java/com/learn/universityjpa/UniversityJpaApplicationTest.java
@@ -0,0 +1,11 @@
+package com.learn.universityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class UniversityJpaApplicationTest {
+    @Test
+    void contextLoads() {}
+}
