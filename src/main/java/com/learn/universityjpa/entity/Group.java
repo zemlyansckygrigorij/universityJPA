9ba92394f27ae26f4477dfaadf4d0300c5089ca2
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-10-15 14:35:15 +0500

    updated class  Group  add relationship @OneToMany to table student

--- src/main/java/com/learn/universityjpa/entity/Group.java
+++ src/main/java/com/learn/universityjpa/entity/Group.java
@@ -9,17 +9,15 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "groups")
 public class Group {
     @Id
-    @EqualsAndHashCode.Include()
     @GeneratedValue
     private Long id;
     @Column(name="name", nullable=false)
     private String name;
     @Column(name="specification", nullable=false)
     private String specification;
-    /*@OneToMany(targetEntity=Student.class, mappedBy="id",cascade=CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Student> students;*/
+    @OneToMany(targetEntity=Student.class, mappedBy="id",cascade=CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Student> students;
 }
