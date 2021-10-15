c24cd8aa0ddf543a8717ccc923cb64699b377603
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-10-15 14:39:15 +0500

    updated class Student add annotation @NoArgsConstructor

--- src/main/java/com/learn/universityjpa/entity/Student.java
+++ src/main/java/com/learn/universityjpa/entity/Student.java
@@ -1,9 +1,11 @@
 package com.learn.universityjpa.entity;
 
+import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "student")
+@NoArgsConstructor
 public class Student  extends Person {
     @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @JoinColumn(name = "id_group", nullable = false)
