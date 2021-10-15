919ad84ad1a366593e25bf7d039cc5aaefef2bc6
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-10-15 14:29:19 +0500

    created class GenderConverter for convert enum type before insert it to database

---
+++ src/main/java/com/learn/universityjpa/entity/GenderConverter.java
@@ -0,0 +1,24 @@
+package com.learn.universityjpa.entity;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class GenderConverter extends org.hibernate.type.EnumType  {
+    public void nullSafeSet(
+            PreparedStatement st,
+            Object value,
+            int index,
+            SharedSessionContractImplementor session)
+            throws HibernateException, SQLException {
+        st.setObject(
+                index,
+                value != null ?
+                        ((Enum) value).name() :
+                        null,
+                Types.OTHER
+        );
+    }
+}
