2ac53b8dc2241fbe3f8dbd55a286c39b1273084b
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-10-15 14:41:11 +0500

    updated class Student add annotation @EqualsAndHashCode(callSuper=false)

--- src/main/java/com/learn/universityjpa/entity/Student.java
+++ src/main/java/com/learn/universityjpa/entity/Student.java
@@ -1,5 +1,6 @@
 package com.learn.universityjpa.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@EqualsAndHashCode(callSuper=false)
 @Table(name = "student")
 @NoArgsConstructor
 public class Student  extends Person {
