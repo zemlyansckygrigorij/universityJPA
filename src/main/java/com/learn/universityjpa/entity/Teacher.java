ac3bfded20bbbf8357018f62d6c15f9170c954ed
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-10-15 14:50:00 +0500

    updated class Teacher add annotations @Getter @Setter @EqualsAndHashCod(callSuper=false)  @NoArgsConstructor

--- src/main/java/com/learn/universityjpa/entity/Teacher.java
+++ src/main/java/com/learn/universityjpa/entity/Teacher.java
@@ -1,10 +1,19 @@
 package com.learn.universityjpa.entity;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper=false)
+@NoArgsConstructor
 @Table(name = "teacher")
 public class Teacher extends Person {
     @Column(name="category", nullable=false)
