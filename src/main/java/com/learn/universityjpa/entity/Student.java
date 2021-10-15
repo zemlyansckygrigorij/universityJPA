07b714779660cabf63924c7fc84d7a96e7f7afd0
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-10-15 14:40:24 +0500

    updated class Student add annotations @Getter @Setter

--- src/main/java/com/learn/universityjpa/entity/Student.java
+++ src/main/java/com/learn/universityjpa/entity/Student.java
@@ -1,9 +1,14 @@
 package com.learn.universityjpa.entity;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "student")
 @NoArgsConstructor
 public class Student  extends Person {
