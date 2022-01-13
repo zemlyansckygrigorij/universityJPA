2e2b5495f72cc636761e620509e23a369e30de2c
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-01-13 15:13:44 +0500

    change interface GroupRepository create method List<Student> findAllStudentFromGroupByid(Long groupId)

--- src/main/java/com/learn/universityjpa/repo/GroupRepository.java
+++ src/main/java/com/learn/universityjpa/repo/GroupRepository.java
@@ -1,9 +1,21 @@
 package com.learn.universityjpa.repo;
 
 import com.learn.universityjpa.entity.Group;
+import com.learn.universityjpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
+    // выбрать всех студентов данной группы
+    @Query("select st from Student st where st.id_group =:groupId ")
+    List<Student> findAllStudentFromGroupByid(Long groupId);
+
+/*
+    @Query("select us from Group us inner join ReferalEntity ref on us.id = ref.partner.id and ref.referal.id = :userId order by us.id nulls first")
+    UserEntity findPartnerByReferalId(Long userId);*/
+    // найти предметы данной группы*
 }
