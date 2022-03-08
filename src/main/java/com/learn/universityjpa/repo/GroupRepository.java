18eb0e58754bfc6a412655eaff81a4f42179e005
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-03-08 13:14:01 +0500

    change interface GroupRepository

--- src/main/java/com/learn/universityjpa/repo/GroupRepository.java
+++ src/main/java/com/learn/universityjpa/repo/GroupRepository.java
@@ -2,20 +2,30 @@ package com.learn.universityjpa.repo;
 
 import com.learn.universityjpa.entity.Group;
 import com.learn.universityjpa.entity.Student;
+import com.learn.universityjpa.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface GroupRepository extends JpaRepository<Group, Long> {
-    // выбрать всех студентов данной группы
-    @Query("select st from Student st where st.id_group =:groupId ")
-    List<Student> findAllStudentFromGroupByid(Long groupId);
-
-/*
-    @Query("select us from Group us inner join ReferalEntity ref on us.id = ref.partner.id and ref.referal.id = :userId order by us.id nulls first")
-    UserEntity findPartnerByReferalId(Long userId);*/
-    // найти предметы данной группы*
+interface GroupRepository extends JpaRepository<Group, Long> {
+    @Query(value
+            = "select * "
+            + "from student "
+            + "where student.id = :studentId and "
+            + " student.id_group = :groupId ",
+            nativeQuery = true)
+    Optional<List<Student>> getStudentByIdFromGroup(@Param("studentId") Long studentId, @Param("groupId") Long groupId);
+    @Query(value
+            = "select * "
+            + "from subject as s inner join group_subject as g "
+            + "     on s.id_subject = g.id_subject "
+            + "where g.id_subject = :subjectId and "
+            + " g.id_group = :groupId ",
+            nativeQuery = true)
+    Optional<List<Subject>> getSubjectByIdFromGroup(@Param("subjectId") Long subjectId, @Param("groupId") Long groupId);
 }
