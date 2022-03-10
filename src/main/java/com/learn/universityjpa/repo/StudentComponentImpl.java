9a2900f9448eb7336ad1c51ad6e787dcce83f399
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-03-10 11:54:23 +0500

    change class  StudentComponentImpl  add methods: findById, findByIdOrDie,commit,findGroup,findAllSubjects,checkSubject

--- src/main/java/com/learn/universityjpa/repo/StudentComponentImpl.java
+++ src/main/java/com/learn/universityjpa/repo/StudentComponentImpl.java
@@ -1,4 +1,45 @@
 package com.learn.universityjpa.repo;
 
-public class StudentComponentImpl {
+import com.learn.universityjpa.entity.Group;
+import com.learn.universityjpa.entity.Student;
+import com.learn.universityjpa.entity.Subject;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class StudentComponentImpl implements StudentComponent {
+    private final StudentRepository repo;
+    @Override
+    public Optional<Student> findById(Long id) {
+        return repo.findById(id);
+    }
+
+    @Override
+    public Student findByIdOrDie(Long id) throws Exception {
+        return this.findById(id)
+                .orElseThrow(() -> new Exception("Student by id '" + id + "' not found"));
+    }
+
+    @Override
+    public Student commit(Student student) {
+        return repo.save(student);
+    }
+
+    @Override
+    public Group findGroup(Student student) {
+        return student.getGroup();
+    }
+
+    @Override
+    public List<Subject> findAllSubjects(Student student) {
+        return student.getGroup().getSubjects();
+    }
+
+    @Override
+    public boolean checkSubject(Student student, Subject subject) {
+        return student.getGroup().getSubjects().contains(subject);
+    }
 }
