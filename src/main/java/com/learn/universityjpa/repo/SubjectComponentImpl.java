d7faf6a73a43d5663d9f966c92035f8fbb4e7478
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-03-31 10:44:43 +0500

    create class SubjectComponentImpl

---
+++ src/main/java/com/learn/universityjpa/repo/SubjectComponentImpl.java
@@ -0,0 +1,41 @@
+package com.learn.universityjpa.repo;
+
+import com.learn.universityjpa.entity.Subject;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class SubjectComponentImpl
+ */
+@RequiredArgsConstructor
+@Component
+public class SubjectComponentImpl implements  SubjectComponent {
+    @Autowired
+    SubjectRepository repo;
+
+    @Override
+    public Optional<Subject> findById(Long id) {
+        return this.repo.findById(id);
+    }
+
+    @Override
+    public Subject findByIdOrDie(Long id) throws Exception {
+        return this.repo.findById(id).orElseThrow(() -> new Exception("Subject by id '" + id + "' not found"));
+    }
+
+    @Override
+    public Subject commit(Subject subject) {
+        return this.repo.save(subject);
+    }
+
+    @Override
+    public List<Subject> findAll() {
+        return this.repo.findAll();
+    }
+}
