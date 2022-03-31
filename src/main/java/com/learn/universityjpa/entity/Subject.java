40b5f9ccf91c3082edcf627364325b7e392bdd4a
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-03-31 10:41:12 +0500

    change class Subject

--- src/main/java/com/learn/universityjpa/entity/Subject.java
+++ src/main/java/com/learn/universityjpa/entity/Subject.java
@@ -3,10 +3,13 @@ package com.learn.universityjpa.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 /**
@@ -21,10 +24,12 @@ import javax.persistence.Table;
 public class Subject {
     @Id
     @GeneratedValue
-    @Column(name = "id_subject", nullable = false)
     private Long id;
-    @Column(name = "name_subject", nullable = false)
+    @Column(name = "name", nullable = false)
     private String name;
     @Column(name = "description", nullable = false)
     private String description;
+
+    @ManyToMany
+    Set<Group> groups;
 }
