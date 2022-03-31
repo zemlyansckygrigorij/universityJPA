17ef9a5fad55a8f24b53ad3d434f267c19a46bd9
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-03-31 10:39:55 +0500

    change class GroupTest

--- src/test/java/com/learn/universityjpa/entity/GroupTest.java
+++ src/test/java/com/learn/universityjpa/entity/GroupTest.java
@@ -1,6 +1,8 @@
 package com.learn.universityjpa.entity;
 
 import com.learn.universityjpa.repo.GroupComponent;
+import com.learn.universityjpa.repo.SubjectComponent;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +12,14 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.*;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
 
@@ -23,67 +31,56 @@ import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.IS
 class GroupTest {
 
     @Autowired
-   private GroupComponent component;
-
+    private GroupComponent component;
+    @Autowired
+    SubjectComponent subjectComponent;
     @Test
-    @Sql(
-            scripts = "/db/sql/entity/Group/clean-up.sql",
-            executionPhase = AFTER_TEST_METHOD,
-            config = @SqlConfig(transactionMode = ISOLATED)
-    )
-    public void makeTest(){
-        Group group = new Group();
-        group.setId(1L);
-        group.setName("Group");
-        group.setSpecification("Specification");
-        Student student = new Student();
-        student.setId(1L);
-        List<Student> list = new ArrayList<>() ;
-        list.add(student);
-        group.setStudents(list);
-
-        assertEquals(group.getId(), 1);
-        assertEquals(group.getName(), "Group");
-        assertEquals(group.getSpecification(), "Specification");
-        assertEquals(group.getStudents().get(0), student);
-        Group group1 = new Group();
-
-        assertNull(group1.getId());
-        assertNull(group1.getName());
-        assertNull(group1.getSpecification());
-        assertNull(group1.getStudents());
-
+    public void checkGroupComponent() {
+        assertNotNull(component);
     }
+
     @Test
     @Sql(
             scripts = "/db/sql/entity/Group/clean-up.sql",
             executionPhase = AFTER_TEST_METHOD,
             config = @SqlConfig(transactionMode = ISOLATED)
     )
-    public void insertTest(){
-       long count = component.findAll().size();
+    public void insertTest() {
+        Subject subject = new Subject();
+        subject.setDescription("Description");
+        subject.setName("Name");
+        subjectComponent.commit(subject);
+        Set<Subject> subjects = new HashSet<>();
+        subjects.add(subject);
+
+
+        long count = component.findAll().size();
+        assertEquals(count, 0);
         Group group = new Group();
         group.setSpecification("Specification");
         group.setName("Name");
-
+        group.setSubjects(subjects);
         component.commit(group);
         Group groupFrom = component.findAll().get(0);
         assertEquals(groupFrom.getSpecification(), "Specification");
         assertEquals(groupFrom.getName(), "Name");
-        assertEquals(count+1, component.findAll().size());
+        assertTrue(groupFrom.getSubjects().contains(subject));
+        assertEquals(count + 1, component.findAll().size());
     }
+
     @Test
     public void shouldNotAllowNullSpecification() {
         Group group = new Group();
         group.setName("Group");
         group.setSpecification(null);
-        assertThrows(Exception.class,()-> component.commit(group));
+        assertThrows(Exception.class, ()-> component.commit(group));
     }
+
     @Test
     public void shouldNotAllowNullName() {
         Group group = new Group();
         group.setName(null);
         group.setSpecification("Specification");
-        assertThrows(Exception.class,()-> component.commit(group));
+        assertThrows(Exception.class, ()-> component.commit(group));
     }
 }
