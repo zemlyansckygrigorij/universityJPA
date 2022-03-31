4309ba38b764095bab61a7e8ef77af1ab6a35d25
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-03-31 10:41:33 +0500

    change class SubjectTest

--- src/test/java/com/learn/universityjpa/entity/SubjectTest.java
+++ src/test/java/com/learn/universityjpa/entity/SubjectTest.java
@@ -1,14 +1,21 @@
 package com.learn.universityjpa.entity;
 
-import com.learn.universityjpa.repo.SubjectRepository;
+
+import com.learn.universityjpa.repo.SubjectComponent;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
 
 
 @SpringBootTest
@@ -17,32 +24,43 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class SubjectTest {
     @Autowired
-    SubjectRepository repo;
+    SubjectComponent subjectComponent;
+
     @Test
-    public void makeTest(){
-        long count = repo.findAll().size();
+    @Sql(
+            scripts = "/db/sql/entity/Subject/clean.sql",
+            executionPhase = AFTER_TEST_METHOD,
+            config = @SqlConfig(transactionMode = ISOLATED)
+    )
+    public void makeTest() {
+
+        long count = subjectComponent.findAll().size();
+
         Subject subject = new Subject();
         subject.setDescription("Description");
         subject.setName("Name");
-        repo.save(subject);
-        Subject subjectFrom = repo.findAll().get(0);
+
+
+        subjectComponent.commit(subject);
+
+        Subject subjectFrom = subjectComponent.findAll().get(0);
         assertEquals(subject, subjectFrom);
         assertEquals(subjectFrom.getDescription(), "Description");
         assertEquals(subjectFrom.getName(), "Name");
-        assertEquals(count+1, repo.findAll().size());
+        assertEquals(count + 1, subjectComponent.findAll().size());
     }
     @Test
     public void shouldNotAllowNullDescription() {
         Subject subject = new Subject();
         subject.setDescription(null);
         subject.setName("Name");
-        assertThrows(Exception.class,()->repo.save(subject));
+        assertThrows(Exception.class, () -> subjectComponent.commit(subject));
     }
     @Test
     public void shouldNotAllowNullName() {
         Subject subject = new Subject();
         subject.setName(null);
-        subject.setDescription("Name");
-        assertThrows(Exception.class,()->repo.save(subject));
+        subject.setDescription("Description");
+        assertThrows(Exception.class, () -> subjectComponent.commit(subject));
     }
 }
