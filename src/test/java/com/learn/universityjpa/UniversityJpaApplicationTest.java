2c2f4e809dd93b07d4382c39d32f9c9142cd3b21
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-03-31 10:42:40 +0500

    change class UniversityJpaApplicationTest

--- src/test/java/com/learn/universityjpa/UniversityJpaApplicationTest.java
+++ src/test/java/com/learn/universityjpa/UniversityJpaApplicationTest.java
@@ -3,7 +3,6 @@ package com.learn.universityjpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class UniversityJpaApplicationTest {
     @Test
