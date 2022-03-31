6537b0810d74e779a9077573fa2a5f65ca4928f1
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-03-31 10:44:09 +0500

    create interface TeacherComponent

---
+++ src/main/java/com/learn/universityjpa/repo/TeacherComponent.java
@@ -0,0 +1,44 @@
+package com.learn.universityjpa.repo;
+
+import com.learn.universityjpa.entity.Teacher;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Компонент работы с данными преподавателями .
+ */
+public interface TeacherComponent {
+    /**
+     * Ищет преподавателя по идентификатору.
+     *
+     * @param id идентификатор преподавателя.
+     * @return преподаватель.
+     */
+    Optional<Teacher> findById(Long id);
+
+    /**
+     * Ищет преподавателя по идентификатору и падает по ошибке, если не нашел.
+     *
+     * @param id идентификаторпреподавателя.
+     * @return преподаватель.
+     */
+    Teacher findByIdOrDie(Long id) throws Exception;
+
+    /**
+     * Сохраняет преподавателя.
+     *
+     * @param teacher преподаватель для сохранения.
+     * @return сохраненная преподаватель.
+     */
+    Teacher commit(Teacher teacher);
+
+    /**
+     * Находит всех преподавателей.
+     *
+     * @return список преподавателей .
+     */
+    List<Teacher> findAll();
+
+
+
+}
