d47dad2f1aaa1f7ef98d94a46ae47d27dc216b96
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-03-31 10:45:03 +0500

    create interface SubjectComponent

---
+++ src/main/java/com/learn/universityjpa/repo/SubjectComponent.java
@@ -0,0 +1,43 @@
+package com.learn.universityjpa.repo;
+
+import com.learn.universityjpa.entity.Subject;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Компонент работы с данными предметами .
+ */
+public interface SubjectComponent {
+
+    /**
+     * Ищет предмет по идентификатору.
+     *
+     * @param id идентификатор группы.
+     * @return предмет.
+     */
+    Optional<Subject> findById(Long id);
+
+    /**
+     * Ищет предмет  по идентификатору и падает по ошибке, если не нашел.
+     *
+     * @param id идентификатор группы.
+     * @return предмет.
+     */
+    Subject findByIdOrDie(Long id) throws Exception;
+
+    /**
+     * Сохраняет предмет.
+     *
+     * @param subject группа для сохранения.
+     * @return сохраненный предмет.
+     */
+    Subject commit(Subject subject);
+    /**
+     * Находит всех предметы.
+     *
+     * @return список предметов.
+     */
+    List<Subject> findAll();
+
+}
