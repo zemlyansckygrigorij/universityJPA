4bde5c698b8eb67fa7705add76b216c4e044365b
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-04-01 16:28:24 +0500

    create file test\recourses\db\sql\insert.sql

---
+++ src/test/resources/db/sql/insert.sql
@@ -0,0 +1,16 @@
+insert into groups(id, name ,specification) values
+                                                (1,'Computer Science LEVEL first','Note: Individual program requirements may exceed the following minima.
+
+Minimum 128 total semester hours required
+
+Minimum 2.000 GPA required
+
+Students must earn a minimum of 64  semester hours in order to receive a bachelor’s degree.'),
+                                                (2,'Computer Science LEVEL second','Note: Individual program requirements may exceed the following minima.
+
+Minimum 120 total semester hours required
+
+Minimum 2.000 GPA required
+
+Students must earn a minimum of 64  semester hours in order to receive a bachelor’s degree.')
+;
