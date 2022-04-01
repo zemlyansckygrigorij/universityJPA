69af721bbbebcfd94b99a1ff0cef70b57f9a6028
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-04-01 16:27:07 +0500

    change interface GroupRepository

--- src/main/java/com/learn/universityjpa/repo/GroupRepository.java
+++ src/main/java/com/learn/universityjpa/repo/GroupRepository.java
@@ -1,8 +1,7 @@
 package com.learn.universityjpa.repo;
 
 import com.learn.universityjpa.entity.Group;
-import com.learn.universityjpa.entity.Student;
-import com.learn.universityjpa.entity.Subject;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,21 +10,20 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+
 @Repository
 interface GroupRepository extends JpaRepository<Group, Long> {
+
     @Query(value
             = "select * "
-            + "from student "
-            + "where student.id = :studentId and "
-            + " student.id_group = :groupId ",
+            + "from groups  "
+            + "where name  LIKE %:nameGroup% ",
             nativeQuery = true)
-    Optional<List<Student>> getStudentByIdFromGroup(@Param("studentId") Long studentId, @Param("groupId") Long groupId);
+    Optional<List<Group>> findByName(@Param("nameGroup")String nameGroup);
+
     @Query(value
-            = "select * "
-            + "from subject as s inner join group_subject as g "
-            + "     on s.id_subject = g.id_subject "
-            + "where g.id_subject = :subjectId and "
-            + " g.id_group = :groupId ",
+            = "select * from groups " +
+            "where id in (select group_id from group_subject where subject_id in (:ids))",
             nativeQuery = true)
-    Optional<List<Subject>> getSubjectByIdFromGroup(@Param("subjectId") Long subjectId, @Param("groupId") Long groupId);
+    Optional<List<Group>> findBySubjects(@Param("ids") long[] ids);
 }
