91bf8c7a652961bfbf3610541a79d542e8c88ccc
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-04-04 16:47:23 +0500

    change class StudentComponentImpl

--- src/main/java/com/learn/universityjpa/repo/StudentComponentImpl.java
+++ src/main/java/com/learn/universityjpa/repo/StudentComponentImpl.java
@@ -2,10 +2,10 @@ package com.learn.universityjpa.repo;
 
 import com.learn.universityjpa.entity.Group;
 import com.learn.universityjpa.entity.Student;
-//import com.learn.universityjpa.entity.Subject;
+import com.learn.universityjpa.entity.Subject;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-//import java.util.List;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -16,7 +16,10 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Component
 public class StudentComponentImpl implements StudentComponent {
+
     private final StudentRepository repo;
+    private final GroupComponent groupComponent;
+
     @Override
     public Optional<Student> findById(Long id) {
         return repo.findById(id);
@@ -30,15 +33,23 @@ public class StudentComponentImpl implements StudentComponent {
 
     @Override
     public Student commit(Student student) {
-        return repo.save(student);
+        return this.repo.save(student);
     }
 
+    public List<Student> findAll() {
+        return  repo.findAll();
+    }
     @Override
     public Group findGroup(Student student) {
         return student.getGroup();
     }
 
- /*   @Override
+    public List<Student> findAllFromGroup(Group group) throws Exception {
+        return  repo.getStudentsFromGroup(group.getId()).orElseThrow(
+                () -> new Exception("Student by id '" + group.getId() + "' not found"));
+    }
+
+   @Override
     public List<Subject> findAllSubjects(Student student) {
         return student.getGroup().getSubjects();
     }
@@ -46,5 +57,5 @@ public class StudentComponentImpl implements StudentComponent {
     @Override
     public boolean checkSubject(Student student, Subject subject) {
         return student.getGroup().getSubjects().contains(subject);
-    }*/
+    }
 }
