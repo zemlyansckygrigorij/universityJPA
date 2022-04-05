1e269e10ad3dd3926d37f911b692a5fb4769528e
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-04-05 14:49:34 +0500

    change interface SubjectRepository  add  Optional<List<Subject>> getSubjectsByName(@Param("nameSubject") String nameSubject);

--- src/main/java/com/learn/universityjpa/repo/SubjectRepository.java
+++ src/main/java/com/learn/universityjpa/repo/SubjectRepository.java
@@ -2,7 +2,12 @@ package com.learn.universityjpa.repo;
 
 import com.learn.universityjpa.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Grigoriy Zemlyanskiy
@@ -11,4 +16,11 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 interface SubjectRepository extends JpaRepository<Subject, Long> {
+
+    @Query(value
+            = "select * "
+            + "from subject  "
+            + "where subject.name LIKE %:nameSubject%",
+            nativeQuery = true)
+    Optional<List<Subject>> getSubjectsByName(@Param("nameSubject") String nameSubject);
 }
