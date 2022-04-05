8648558e0c5aecdf92bd1860b45c0bf19c214117
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-04-05 12:31:55 +0500

    change interface StudentRepository add getStudentsFromGroup(@Param("groupId") Long groupId) and  getStudentsByName(@Param("name") String name)

--- src/main/java/com/learn/universityjpa/repo/StudentRepository.java
+++ src/main/java/com/learn/universityjpa/repo/StudentRepository.java
@@ -2,7 +2,12 @@ package com.learn.universityjpa.repo;
 
 import com.learn.universityjpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Grigoriy Zemlyanskiy
@@ -11,4 +16,16 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query(value
+            = "select * "
+            + "from student  "
+            + "where id_group = :groupId ",
+            nativeQuery = true)
+    Optional<List<Student>> getStudentsFromGroup(@Param("groupId") Long groupId);
+
+    @Query("SELECT s FROM Student s " +
+            "WHERE s.firstName LIKE %:name% " +
+            "or s.secondName LIKE %:name% " +
+            "or s.lastName LIKE %:name%")
+    Optional<List<Student>> getStudentsByName(@Param("name") String name);
 }
