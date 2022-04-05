2f7218c273ed4097f87429d7527f0c3dc38971be
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-04-05 14:47:48 +0500

    change interface TeacherRepository add  Optional<List<Teacher>> getTeachersByName(@Param("name") String name);

--- src/main/java/com/learn/universityjpa/repo/TeacherRepository.java
+++ src/main/java/com/learn/universityjpa/repo/TeacherRepository.java
@@ -2,7 +2,13 @@ package com.learn.universityjpa.repo;
 
 import com.learn.universityjpa.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * @author Grigoriy Zemlyanskiy
  * @version 1.0
@@ -10,4 +16,9 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    @Query("SELECT t FROM  Teacher t " +
+            "WHERE t.firstName LIKE %:name% " +
+            "or t.secondName LIKE %:name% " +
+            "or t.lastName LIKE %:name%")
+    Optional<List<Teacher>> getTeachersByName(@Param("name") String name);
 }
