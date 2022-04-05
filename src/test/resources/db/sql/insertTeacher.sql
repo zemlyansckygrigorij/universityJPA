f0ac0a3ab40b77cce517944497eebc47166e2e2d
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-04-05 14:52:30 +0500

    create file /db/sql/insertTeacher.sql

---
+++ src/test/resources/db/sql/insertTeacher.sql
@@ -0,0 +1,12 @@
+insert into teacher(id, first_name ,second_name,last_name,gender,date_birth,category) values
+(1,'Oliver','Alexander','Williams','MALE','02-03-99','first'),
+(2,'Harry','Quinn','Peters','MALE','01-04-00','Second'),
+(3,'Jack','Mendoza','Gibson','MALE','02-05-01','Third'),
+(4,'Jacob','Allen','Martin','MALE','12-05-00','first'),
+(5,'Charley','Clarke','Jordan','MALE','03-06-02','Second'),
+(6,'Thomas','Elliott','Jackson','MALE','04-07-98','Third'),
+(7,'George','Mills','Grant','MALE','05-08-00','first'),
+(8,'Oscar','Bowman','Beatley','MALE','06-09-01','Second'),
+(9,'Zachary','Mason','Davis','MALE','07-10-02','Third')
+
+;
