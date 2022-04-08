719114beb4b4861219944eb8adc8d5253501cb6f
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-04-08 17:09:57 +0500

    create interface WriteDataToExcel

---
+++ src/main/java/com/learn/universityjpa/service/excel/WriteDataToExcel.java
@@ -0,0 +1,14 @@
+package com.learn.universityjpa.service.excel;
+
+import java.io.IOException;
+
+/**
+ * Компонент для записи данных в excel файлы .
+ */
+public interface WriteDataToExcel {
+
+    /**
+     * создает файл с данными.
+     */
+   void create() throws IOException;
+}
