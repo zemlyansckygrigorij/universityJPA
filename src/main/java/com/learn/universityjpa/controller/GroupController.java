d88c19d79e6029967552790c237b0b386736dd9a
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-05-05 15:24:17 +0500

    create class GroupController for interaction with web

---
+++ src/main/java/com/learn/universityjpa/controller/GroupController.java
@@ -0,0 +1,94 @@
+package com.learn.universityjpa.controller;
+
+import com.learn.universityjpa.controller.model.request.GroupRequest;
+import com.learn.universityjpa.controller.model.response.GroupResponse;
+import com.learn.universityjpa.controller.model.response.SubjectResponse;
+import com.learn.universityjpa.entity.Group;
+import com.learn.universityjpa.repo.GroupComponent;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class GroupController
+ * для работы с web сайтом /groups
+ */
+@RestController
+@Validated
+@Tag(name = "API работы с группами")
+@RequestMapping("/groups")
+@RequiredArgsConstructor
+public class GroupController {
+    private final GroupComponent groupComponent;
+
+    @GetMapping()
+    public List<GroupResponse> getAllGroups() {
+        return groupComponent.findAll().stream().map(
+                (x)-> new GroupResponse(x)
+        ).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public GroupResponse getGroupById(
+            @PathVariable(name = "id") final long id
+    ) throws Exception {
+        return new GroupResponse(groupComponent.findByIdOrDie(id));
+    }
+
+    @GetMapping("/{id}/subjects")
+    public List<SubjectResponse> getSubjectsByGroupId(
+            @PathVariable(name = "id") final long id
+    ) throws Exception {
+        return groupComponent.findByIdOrDie(id).getSubjects().stream().map(
+                (x)-> new SubjectResponse(x)
+        ).collect(Collectors.toList());
+    }
+    @PostMapping()
+    public Group  createGroup(@RequestBody GroupRequest request) {
+        Group group = request.groupBuilder();
+        groupComponent.commit(group);
+        return group;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(
+            @PathVariable(name = "id") final long id
+    ) throws Exception {
+        groupComponent.deleteGroupById(id);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
