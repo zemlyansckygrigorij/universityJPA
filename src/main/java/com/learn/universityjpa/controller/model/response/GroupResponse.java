e0fd19818b82594698244f000e5a2e0b7cd7e806
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-05-05 15:25:08 +0500

    create class GroupResponse for interaction with web

---
+++ src/main/java/com/learn/universityjpa/controller/model/response/GroupResponse.java
@@ -0,0 +1,34 @@
+package com.learn.universityjpa.controller.model.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.learn.universityjpa.entity.Group;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Данные группы.
+ */
+@Schema(description = "Данные группы")
+@Data
+@AllArgsConstructor
+public class GroupResponse {
+
+    @Schema(description = "Идентификатор")
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("name")
+    @Schema(description = "Название")
+    private String name;
+
+    @Schema(description = "Описание")
+    @JsonProperty("specification")
+    private String specification;
+
+    public GroupResponse(Group group) {
+     this.id = group.getId();
+     this.name = group.getName();
+     this.specification = group.getSpecification();
+    }
+}
