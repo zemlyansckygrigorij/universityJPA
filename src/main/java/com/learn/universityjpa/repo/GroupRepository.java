9487b33712f45fab1931e45f79e93a37a8ee4d51
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-05-12 11:41:40 +0500

    update interface GroupRepository add method updateGroupById

--- src/main/java/com/learn/universityjpa/repo/GroupRepository.java
+++ src/main/java/com/learn/universityjpa/repo/GroupRepository.java
@@ -3,12 +3,13 @@ package com.learn.universityjpa.repo;
 import com.learn.universityjpa.entity.Group;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
+import javax.transaction.Transactional;
 
 
 @Repository
@@ -26,4 +27,9 @@ interface GroupRepository extends JpaRepository<Group, Long> {
             "where id in (select group_id from group_subject where subject_id in (:ids))",
             nativeQuery = true)
     Optional<List<Group>> findBySubjects(@Param("ids") long[] ids);
+
+    @Modifying
+    @Transactional // @Modifying annotation should be wrapped up with @Transactional
+    @Query("update Group g set g.name = ?1 , g.specification = ?2  where g.id = ?3")
+    int updateGroupById(String name, String specification, Long id);
 }
