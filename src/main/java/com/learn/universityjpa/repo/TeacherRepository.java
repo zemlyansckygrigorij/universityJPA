1abe506e302397c83ba43a883cd58d1a587f51b4
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-06-22 15:46:14 +0500

    update interface TeacherRepository add method updateTeacherById

--- src/main/java/com/learn/universityjpa/repo/TeacherRepository.java
+++ src/main/java/com/learn/universityjpa/repo/TeacherRepository.java
@@ -1,13 +1,16 @@
 package com.learn.universityjpa.repo;
 
 import com.learn.universityjpa.entity.Teacher;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import javax.transaction.Transactional;
 
 /**
  * @author Grigoriy Zemlyanskiy
@@ -21,4 +24,17 @@ interface TeacherRepository extends JpaRepository<Teacher, Long> {
             "or t.secondName LIKE %:name% " +
             "or t.lastName LIKE %:name%")
     Optional<List<Teacher>> getTeachersByName(@Param("name") String name);
+
+    @Modifying
+    @Transactional // @Modifying annotation should be wrapped up with @Transactional
+    @Query("update Group g set g.name = ?1 , g.specification = ?2  where g.id = ?3")
+    int updateTeacherById(
+            String firstName,
+            String secondName,
+            String lastName,
+            String dateBirthday,
+            String category,
+            String gender,
+            Long id
+    );
 }
