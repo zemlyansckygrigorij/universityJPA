1dc7f8fded545951537dfe189f6ef4d4218cfc90
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-06-22 15:47:10 +0500

    update interface StudentRepository add method int updateStudentById

--- src/main/java/com/learn/universityjpa/repo/StudentRepository.java
+++ src/main/java/com/learn/universityjpa/repo/StudentRepository.java
@@ -1,13 +1,23 @@
 package com.learn.universityjpa.repo;
 
+import com.learn.universityjpa.entity.Gender;
+import com.learn.universityjpa.entity.Group;
 import com.learn.universityjpa.entity.Student;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+
+
 
 /**
  * @author Grigoriy Zemlyanskiy
@@ -28,4 +38,24 @@ interface StudentRepository extends JpaRepository<Student, Long> {
             "or s.secondName LIKE %:name% " +
             "or s.lastName LIKE %:name%")
     Optional<List<Student>> getStudentsByName(@Param("name") String name);
+
+    @Modifying
+    @Transactional // @Modifying annotation should be wrapped up with @Transactional
+    @Query("update Student s set " +
+            " s.firstName = ?2, " +
+            " s.secondName = ?3," +
+            " s.lastName = ?4," +
+            " s.gender = ?5 ," +
+            " s.group = ?6, " +
+            " s.dateBirth = ?7 " +
+            "  where s.id = ?1 ")
+    int updateStudentById(
+            Long id,
+            String firstName,
+            String secondName,
+            String lastName,
+            Gender gender,
+            Group group,
+            Date dateBirth
+           );
 }
