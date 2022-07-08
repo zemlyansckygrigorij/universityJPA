63a8e210481a3fc92b8da5ff2195d61b89e41855
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-08 12:24:15 +0500

    update class GroupRequest set @Getter @Setter delete method groupBuilder

--- src/main/java/com/learn/universityjpa/controller/model/request/GroupRequest.java
+++ src/main/java/com/learn/universityjpa/controller/model/request/GroupRequest.java
@@ -5,23 +5,28 @@ import com.learn.universityjpa.entity.Group;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Запрос по данным группы.
  */
+
 @Schema(description = "Данные группы")
 @Data
+@Getter
+@Setter
 @AllArgsConstructor
 public class GroupRequest {
-    @Schema(description = "Идентификатор")
+    @Schema(description = "Идентификатор группы")
     @JsonProperty("id")
     private Long id;
 
+    @Schema(description = "Название группы")
     @JsonProperty("name")
-    @Schema(description = "Название")
     private String name;
 
-    @Schema(description = "Описание")
+    @Schema(description = "Описание группы")
     @JsonProperty("specification")
     private String specification;
 
@@ -29,11 +34,5 @@ public class GroupRequest {
         this.id = group.getId();
         this.name = group.getName();
         this.specification = group.getSpecification();
-    }
-    public Group groupBuilder() {
-        Group group = new Group();
-        group.setName(this.name);
-        group.setSpecification(this.specification);
-        return group;
     }
 }
