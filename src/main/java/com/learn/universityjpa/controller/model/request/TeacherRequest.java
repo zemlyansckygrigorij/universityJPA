310330df9d898adbf5af24edf38b111d42f315a4
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-08 12:26:42 +0500

    create class TeacherRequest

---
+++ src/main/java/com/learn/universityjpa/controller/model/request/TeacherRequest.java
@@ -0,0 +1,47 @@
+package com.learn.universityjpa.controller.model.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import java.util.Date;
+
+/**
+ * Запрос по данным преподавателя.
+ */
+@Schema(description = "Данные о преподавателе")
+@Data
+@Getter
+@AllArgsConstructor
+public class TeacherRequest {
+    @Schema(description = "Идентификатор преподавателя")
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("firstName")
+    @Schema(description = "Имя преподавателя")
+    private String firstName;
+
+    @Schema(description = "Отчество преподавателя")
+    @JsonProperty("secondName")
+    private String secondName;
+
+    @Schema(description = "Фамилия преподавателя")
+    @JsonProperty("lastName")
+    private String lastName;
+
+    @Schema(description = "Дата рождения преподавателя")
+    @JsonProperty("dateBirth")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date dateBirth;
+
+    @Schema(description = "Пол преподавателя")
+    @JsonProperty("gender")
+    private String gender;
+
+    @Schema(description = "Категория преподавателя")
+    @JsonProperty("category")
+    private String category;
+}
