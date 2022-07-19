b12165fc86bdc6b9329f99c7176d017e8f461788
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 13:26:21 +0500

    update class Group subjects @ManyToMany=> @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY), add property List<Student> students

--- src/main/java/com/learn/universityjpa/entity/Group.java
+++ src/main/java/com/learn/universityjpa/entity/Group.java
@@ -4,15 +4,19 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
@@ -27,19 +31,25 @@ import javax.persistence.Table;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "groups")
 public class Group {
+
     @Id
     @EqualsAndHashCode.Include()
     @GeneratedValue
-    @Column(name = "id")
     private Long id;
+
     @Column(name = "name", nullable = false)
     private String name;
+
     @Column(name = "specification", nullable = false)
     private String specification;
-    @ManyToMany
+
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
     @JoinTable(
             name = "group_subject",
             joinColumns = @JoinColumn(name = "group_id"),
             inverseJoinColumns = @JoinColumn(name = "subject_id"))
-    List<Subject> subjects;
+    List<Subject> subjects = new ArrayList<>();
+
+    @OneToMany(mappedBy = "group", orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<Student> students = new ArrayList<>();
 }
