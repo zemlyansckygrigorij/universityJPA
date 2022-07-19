ca9667e3ba5edcd9b0e5976a5c56e4a060623b65
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 13:43:12 +0500

    update class StudentComponentImplTest change methods findById and deleteStudentById

--- src/test/java/com/learn/universityjpa/repo/StudentComponentImplTest.java
+++ src/test/java/com/learn/universityjpa/repo/StudentComponentImplTest.java
@@ -44,13 +44,13 @@ class StudentComponentImplTest {
     @Autowired
     private GroupComponent groupComponent;
 
-    @DisplayName("Проверка подключения элемента component")
+    @DisplayName("1. Проверка подключения элемента component.")
     @Test
     public void checkStudentComponentNotNull() {
         assertNotNull(component);
     }
 
-    @DisplayName("Проверка поиска студента по Id")
+    @DisplayName("2. Проверка поиска студента по Id.")
     @SqlTest
     void findById() {
         Student student = component.findById(1L).get();
@@ -61,22 +61,23 @@ class StudentComponentImplTest {
         assertEquals("Abbe", student.getLastName());
         assertEquals(Gender.MALE, student.getGender());
         assertEquals(1, student.getGroup().getId());
+        assertEquals("1999-03-02", new SimpleDateFormat("yyyy-MM-dd").format(student.getDateBirth()));
     }
 
-    @DisplayName("Проверка поиска студента по Id если такого студента нет выкидывается исключение")
+    @DisplayName("3. Проверка поиска студента по Id если такого студента нет выкидывается исключение.")
     @SqlTest
     void findByIdOrDie() {
         assertThrows(PersonNotFoundException.class, ()-> component.findByIdOrDie(140L));
     }
 
 
-    @DisplayName("Проверка поиска всех студентов")
+    @DisplayName("4. Проверка поиска всех студентов.")
     @SqlTest
     void findAll() {
         assertEquals(72 , component.findAll().size());
     }
 
-    @DisplayName("Проверка поиска группы по студенту")
+    @DisplayName("5. Проверка поиска группы по студенту.")
     @SqlTest
     void findGroup() throws Exception {
         Group group = groupComponent.findByIdOrDie(1L);
@@ -88,21 +89,21 @@ class StudentComponentImplTest {
 
     }
 
-    @DisplayName("Проверка поиска всех студентов группы")
+    @DisplayName("6. Проверка поиска всех студентов группы.")
     @SqlTest
     void findAllFromGroup() throws Exception {
         Group group = groupComponent.findByIdOrDie(1L);
         assertEquals(36, component.findAllFromGroup(group).size());
     }
 
-    @DisplayName("Проверка поиска всех предметов данного студента")
+    @DisplayName("7. Проверка поиска всех предметов данного студента.")
     @SqlTest
     void findAllSubjects() throws Exception {
         Student student = component.findByIdOrDie(1L);
         assertEquals(4, component.findAllSubjects(student).size());
     }
 
-    @DisplayName("Проверка наличия предмета у данного студента")
+    @DisplayName("8. Проверка наличия предмета у данного студента.")
     @SqlTest
     void checkSubject() throws Exception {
         Student student = component.findByIdOrDie(1L);
@@ -112,7 +113,7 @@ class StudentComponentImplTest {
         assertFalse(component.checkSubject(student2, subject));
     }
 
-    @DisplayName("Проверка поиска студента по имени")
+    @DisplayName("9. Проверка поиска студента по имени.")
     @SqlTest
     void getStudentsByName() throws Exception {
         List<Student> students = component.getStudentsByName("Dav");
@@ -120,17 +121,34 @@ class StudentComponentImplTest {
         assertEquals(72, component.findAll().size());
     }
 
-    @DisplayName("Проверка удаления студента по id")
-    @SqlTest
+    @DisplayName("10. Проверка удаления студента по id.")
+    @Test
+    @SqlGroup({
+            @Sql(
+                    scripts = "/db/sql/clean.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insert.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insertStudent.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED))
+    })
     void deleteStudentById() throws Exception {
+        int count = component.findAll().size();
         Student student = component.findByIdOrDie(1L);
         assertNotNull(student);
         component.deleteStudentById(1L);
+
         Optional<Student> studentNew = component.findById(1L);
         assertTrue(studentNew.isEmpty());
+        assertEquals(count - 1, component.findAll().size());
     }
 
-    @DisplayName("Проверка изменения данных студента по id")
+    @DisplayName("11. Проверка изменения данных студента по id.")
     @Test
     @SqlGroup({
             @Sql(
