21581f41fcc9360e19529753f893a2eb20f349c0
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 14:05:08 +0500

    update file clean.sql add delete from teacher_subject where id >0;

--- src/test/resources/db/sql/clean.sql
+++ src/test/resources/db/sql/clean.sql
@@ -1,4 +1,5 @@
 delete from group_subject where id >0;
+delete from teacher_subject where id >0;
 delete from student where id > 0;
 delete from groups where id > 0;
 delete from subject where id > 0;
