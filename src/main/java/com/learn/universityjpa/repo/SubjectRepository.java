22e4a672e5e463feb1a772c075b297e6052ee9f7
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 13:52:58 +0500

    update interface SubjectRepository add method updateSubjectById

--- src/main/java/com/learn/universityjpa/repo/SubjectRepository.java
+++ src/main/java/com/learn/universityjpa/repo/SubjectRepository.java
@@ -2,12 +2,16 @@ package com.learn.universityjpa.repo;
 
 import com.learn.universityjpa.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 
 /**
  * @author Grigoriy Zemlyanskiy
@@ -23,4 +27,16 @@ interface SubjectRepository extends JpaRepository<Subject, Long> {
             + "where subject.name LIKE %:nameSubject%",
             nativeQuery = true)
     Optional<List<Subject>> getSubjectsByName(@Param("nameSubject") String nameSubject);
+
+    @Modifying
+    @Transactional // @Modifying annotation should be wrapped up with @Transactional
+    @Query("update Subject s set " +
+            " s.name = ?2, " +
+            " s.description = ?3" +
+            "  where s.id = ?1 ")
+    int updateSubjectById(
+            Long id,
+            String name,
+            String description
+    );
 }
