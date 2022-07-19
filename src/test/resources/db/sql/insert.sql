1d1162725dc6481759dd17845d76014a5c2b19e0
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 14:06:25 +0500

    update file insert.sql

--- src/test/resources/db/sql/insert.sql
+++ src/test/resources/db/sql/insert.sql
@@ -5,8 +5,9 @@ Minimum 128 total semester hours required
 
 Minimum 2.000 GPA required
 
-Students must earn a minimum of 64  semester hours in order to receive a bachelor’s degree.'),
-                                                (2,'Computer Science LEVEL second','Note: Individual program requirements may exceed the following minima.
+Students must earn a minimum of 64  semester hours in order to receive a bachelor’s degree.');
+
+insert into groups(id, name ,specification) values (2,'Computer Science LEVEL second','Note: Individual program requirements may exceed the following minima.
 
 Minimum 120 total semester hours required
 
