3d3bd3cdf35e15aa73f7ab6ad641341d1be5e14f
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 13:58:30 +0500

    update class TeacherComponentImpl add methods: deleteTeacherById, updateTeacherById, findAllSubjects, checkSubject, addSubject(Teacher teacher, Subject subject), deleteSubject(Teacher teacher, Subject subject), deleteSubject(long idTeacher, long idSubject), addSubject(long idTeacher, long idSubject)

--- src/main/java/com/learn/universityjpa/repo/TeacherComponentImpl.java
+++ src/main/java/com/learn/universityjpa/repo/TeacherComponentImpl.java
@@ -1,11 +1,13 @@
 package com.learn.universityjpa.repo;
 
+import com.learn.universityjpa.entity.Subject;
 import com.learn.universityjpa.entity.Teacher;
 import com.learn.universityjpa.exceptions.PersonNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,10 +19,12 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Component
 public class TeacherComponentImpl implements TeacherComponent {
-
+    @Autowired
+    private  SubjectComponent   subjectComponent;
     @Autowired
     TeacherRepository repo;
-
+    @Autowired
+    SubjectRepository subjectRepo;
     @Override
     public Optional<Teacher> findById(Long id) {
         return this.repo.findById(id);
@@ -44,7 +48,75 @@ public class TeacherComponentImpl implements TeacherComponent {
 
     @Override
     public List<Teacher> getTeachersByName(String name) throws Exception {
-        return repo.getTeachersByName(name).orElseThrow(
+        return  this.repo.getTeachersByName(name).orElseThrow(
                 () -> new PersonNotFoundException());
+    }
+
+    @Override
+    public void deleteTeacherById(Long id) {
+        this.repo.deleteById(id);
+    }
+
+    @Override
+    public void updateTeacherById(Long id, Teacher teacher) {
+        int result = this.repo.updateTeacherById(
+                teacher.getFirstName(),
+                teacher.getSecondName(),
+                teacher.getLastName(),
+                teacher.getDateBirth(),
+                teacher.getCategory(),
+                teacher.getGender(),
+                id);
+    }
+
+    @Override
+    public List<Subject> findAllSubjects(Teacher teacher) {
+        return teacher.getSubjects();
+    }
+
+    @Override
+    public boolean checkSubject(Teacher teacher, Subject subject) {
+        return teacher.getSubjects().contains(subject);
+    }
+
+    @Override
+    public Subject addSubject(Teacher teacher, Subject subject) {
+        if (teacher.getSubjects().contains(subject)) {
+            return subject;
+        }
+        subject.getTeachers().add(teacher);
+        teacher.getSubjects().add(subject);
+        this.repo.save(teacher);
+        this.subjectRepo.save(subject);
+        return subject;
+    }
+    public Subject addSubject(long idTeacher, long idSubject) throws Exception {
+        Teacher teacher =  findByIdOrDie(idTeacher);
+        Subject subject = subjectComponent.findByIdOrDie(idSubject);
+        teacher.getSubjects().add(subject);
+        subject.getTeachers().add(teacher);
+        this.repo.save(teacher);
+        this.subjectRepo.save(subject);
+        return subject;
+    }
+    public Subject deleteSubject(long idTeacher, long idSubject) throws Exception {
+        Teacher teacher = findByIdOrDie(idTeacher);
+        Subject subject = subjectComponent.findByIdOrDie(idSubject);
+        subject.getTeachers().remove(teacher);
+        teacher.getSubjects().remove(subject);
+        this.subjectRepo.save(subject);
+        this.repo.save(teacher);
+        return subject;
+    }
+    @Override
+    public Subject deleteSubject(Teacher teacher, Subject subject) throws ParseException {
+        if (!teacher.getSubjects().contains(subject)) {
+            return subject;
+        }
+        subject.getTeachers().remove(teacher);
+        teacher.getSubjects().remove(subject);
+        this.subjectRepo.save(subject);
+        this.repo.save(teacher);
+        return subject;
     }
 }
