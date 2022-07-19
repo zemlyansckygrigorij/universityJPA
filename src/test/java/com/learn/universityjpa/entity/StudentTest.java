cf652f626c121082bc2115d7788d6d16f954dd03
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 13:47:24 +0500

    update class StudentTest change methods makeTest and shouldNotAllowNullGroup. Add methods: shouldNotAllowNullFirstName, shouldNotAllowNullSecondName, shouldNotAllowNullLastName, shouldNotAllowNullGender, shouldNotAllowNullDateBirth

--- src/test/java/com/learn/universityjpa/entity/StudentTest.java
+++ src/test/java/com/learn/universityjpa/entity/StudentTest.java
@@ -2,13 +2,16 @@ package com.learn.universityjpa.entity;
 
 import com.learn.universityjpa.repo.GroupComponent;
 import com.learn.universityjpa.repo.StudentComponent;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,40 +35,47 @@ class StudentTest {
     @Autowired
     private GroupComponent groupComponent;
 
+    @DisplayName("1. Проверка вставки студента.")
     @Test
-    @Sql(
-            scripts = "/db/sql/entity/Student/insert.sql",
-            executionPhase = BEFORE_TEST_METHOD,
-            config = @SqlConfig(transactionMode = ISOLATED)
-    )
-    public void makeTest() {
-        assertNotNull(studentComponent);
-        assertNotNull(groupComponent);
+    @SqlGroup({
+            @Sql(
+                    scripts = "/db/sql/clean.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insert.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insertStudent.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED))
+    })
+    public void makeTest() throws Exception {
+
         Group group = groupComponent.findAll().get(0);
-        assertEquals(group.getId(), 1);
-        assertEquals(group.getName(), "GroupTest");
-        assertEquals(group.getSpecification(), "DescriptionTest");
         Date birth = new Date();
         Student student = new Student();
         student.setDateBirth(birth);
-        student.setFirstName("Jhon");
+        student.setFirstName("Jhon1");
         student.setGender(Gender.MALE);
-        student.setSecondName("Frank");
-        student.setLastName("Kennedy");
-        student.setId(2L);
+        student.setSecondName("Frank1");
+        student.setLastName("Kennedy1");
+
         student.setGroup(group);
-        assertEquals(studentComponent.findAll().size(), 0);
+        assertEquals(studentComponent.findAll().size(), 72);
         studentComponent.commit(student);
-        assertEquals(studentComponent.findAll().size(), 1);
-        Student studentFromTable = studentComponent.findAll().get(0);
+        assertEquals(studentComponent.findAll().size(), 73);
+        Student studentFromTable = studentComponent.getStudentsByName("Jhon1").get(0);
         assertNotNull(studentFromTable);
         assertEquals(studentFromTable.getGroup(), group);
-        assertEquals(studentFromTable.getFirstName(), "Jhon");
-        assertEquals(studentFromTable.getSecondName(), "Frank");
-        assertEquals(studentFromTable.getLastName(), "Kennedy");
+        assertEquals(studentFromTable.getFirstName(), "Jhon1");
+        assertEquals(studentFromTable.getSecondName(), "Frank1");
+        assertEquals(studentFromTable.getLastName(), "Kennedy1");
         assertEquals(studentFromTable.getGender(), Gender.MALE);
     }
 
+    @DisplayName("2. Проверка вставки студента c отсутствием группы.")
     @Test
     public void shouldNotAllowNullGroup() {
         Date birth = new Date();
@@ -75,8 +85,160 @@ class StudentTest {
         student.setGender(Gender.MALE);
         student.setSecondName("Frank");
         student.setLastName("Kennedy");
-        student.setId(2L);
-        student.setGroup(null);
-        assertThrows(Exception.class, ()-> studentComponent.commit(student));
+        assertThrows(DataIntegrityViolationException.class, ()-> studentComponent.commit(student));
     }
+
+    @DisplayName("3. Проверка вставки студента c отсутствием имени.")
+    @Test
+    @SqlGroup({
+            @Sql(
+                    scripts = "/db/sql/clean.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insert.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insertStudent.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED))
+    })
+    public void shouldNotAllowNullFirstName() throws Exception {
+        Group group = groupComponent.findAll().get(0);
+        Date birth = new Date();
+        Student student = new Student();
+        student.setDateBirth(birth);
+        student.setGender(Gender.MALE);
+        student.setSecondName("Frank");
+        student.setLastName("Kennedy");
+        student.setId(2L);
+        student.setGroup(group);
+        studentComponent.commit(student);
+        assertThrows(DataIntegrityViolationException.class, ()-> studentComponent.getStudentsByName("Kennedy").get(0));
+
+    }
+
+    @DisplayName("4. Проверка вставки студента c отсутствием отчества.")
+    @Test
+    @SqlGroup({
+            @Sql(
+                    scripts = "/db/sql/clean.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insert.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insertStudent.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED))
+    })
+    public void shouldNotAllowNullSecondName() {
+        Group group = groupComponent.findAll().get(0);
+        Date birth = new Date();
+        Student student = new Student();
+        student.setDateBirth(birth);
+        student.setFirstName("Jhon");
+        student.setGender(Gender.MALE);
+        student.setLastName("Kennedy");
+        student.setId(2L);
+        student.setGroup(group);
+        studentComponent.commit(student);
+        assertThrows(DataIntegrityViolationException.class,
+                ()-> studentComponent.getStudentsByName("Kennedy").get(0));
+    }
+    @DisplayName("5. Проверка вставки студента c отсутствием фамилии.")
+    @Test
+    @SqlGroup({
+            @Sql(
+                    scripts = "/db/sql/clean.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insert.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insertStudent.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED))
+    })
+    public void shouldNotAllowNullLastName() {
+        Group group = groupComponent.findAll().get(0);
+        Date birth = new Date();
+        Student student = new Student();
+        student.setDateBirth(birth);
+        student.setFirstName("Jhon");
+        student.setGender(Gender.MALE);
+        student.setSecondName("Frank");
+        student.setId(2L);
+        student.setGroup(group);
+        studentComponent.commit(student);
+        assertThrows(DataIntegrityViolationException.class,
+                ()-> studentComponent.getStudentsByName("Frank").get(0));
+    }
+
+    @DisplayName("6. Проверка вставки студента c отсутствием пола.")
+    @Test
+    @SqlGroup({
+            @Sql(
+                    scripts = "/db/sql/clean.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insert.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insertStudent.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED))
+    })
+    public void shouldNotAllowNullGender() {
+        Group group = groupComponent.findAll().get(0);
+        Date birth = new Date();
+        Student student = new Student();
+        student.setDateBirth(birth);
+        student.setFirstName("Jhon");
+        student.setSecondName("Frank");
+        student.setLastName("Kennedy");
+        student.setId(2L);
+        student.setGroup(group);
+        studentComponent.commit(student);
+        assertThrows(DataIntegrityViolationException.class,
+                ()-> studentComponent.getStudentsByName("Frank").get(0));
+    }
+
+    @DisplayName("7. Проверка вставки студента c отсутствием даты рождения.")
+    @Test
+    @SqlGroup({
+            @Sql(
+                    scripts = "/db/sql/clean.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insert.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insertStudent.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED))
+    })
+    public void shouldNotAllowNullDateBirth() {
+        Group group = groupComponent.findAll().get(0);
+        Student student = new Student();
+        student.setFirstName("Jhon");
+        student.setGender(Gender.MALE);
+        student.setSecondName("Frank");
+        student.setLastName("Kennedy");
+        student.setId(2L);
+        student.setGroup(group);
+        studentComponent.commit(student);
+        assertThrows(DataIntegrityViolationException.class,
+                ()-> studentComponent.getStudentsByName("Frank").get(0));
+    }
+
 }
