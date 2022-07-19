7347b540caa1623fbca795a2e43777483f31ccdc
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 14:00:03 +0500

    update class Teacher add property  List<Subject> subjects

--- src/main/java/com/learn/universityjpa/entity/Teacher.java
+++ src/main/java/com/learn/universityjpa/entity/Teacher.java
@@ -1,13 +1,22 @@
 package com.learn.universityjpa.entity;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
 
 /**
  * @author Grigoriy Zemlyanskiy
@@ -17,13 +26,19 @@ import javax.persistence.Table;
 @Entity
 @Getter
 @Setter
-@EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 @Table(name = "teacher")
 public class Teacher extends Person {
 
     @Column(name = "category", nullable = false)
     private String category;
+
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "teacher_subject",
+            joinColumns = @JoinColumn(name = "teacher_id"),
+            inverseJoinColumns = @JoinColumn(name = "subject_id"))
+    List<Subject> subjects = new ArrayList<>();
     @Override
     public String toString() {
         return "teacher {" +
