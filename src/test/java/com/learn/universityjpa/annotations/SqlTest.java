70a2ffc5fd17cfa2284458ab599d7755acf4d436
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 14:04:24 +0500

    update @interface SqlTest add /db/sql/insertTS.sql

--- src/test/java/com/learn/universityjpa/annotations/SqlTest.java
+++ src/test/java/com/learn/universityjpa/annotations/SqlTest.java
@@ -49,6 +49,10 @@ import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.IS
                 executionPhase = BEFORE_TEST_METHOD,
                 config = @SqlConfig(transactionMode = ISOLATED)),
         @Sql(
+                scripts = "/db/sql/insertTS.sql ",
+                executionPhase = BEFORE_TEST_METHOD,
+                config = @SqlConfig(transactionMode = ISOLATED)),
+        @Sql(
                 scripts = "/db/sql/clean.sql ",
                 executionPhase = AFTER_TEST_METHOD,
                 config = @SqlConfig(transactionMode = ISOLATED))
