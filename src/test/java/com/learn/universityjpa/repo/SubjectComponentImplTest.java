46d97c21aa23d24eee37228a6bcf90e8b3bb593e
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 13:50:56 +0500

    update class SubjectComponentImplTest add methods: deleteSubjectById, updateSubjectById.

--- src/test/java/com/learn/universityjpa/repo/SubjectComponentImplTest.java
+++ src/test/java/com/learn/universityjpa/repo/SubjectComponentImplTest.java
@@ -10,15 +10,22 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.ParseException;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -31,14 +38,14 @@ class SubjectComponentImplTest {
     @Autowired
     private GroupComponent groupComponent;
 
-    @DisplayName("Проверка подключения элемента component")
+    @DisplayName("1. Проверка подключения элемента component.")
     @Test
     public void checkSubjectComponentNotNull() {
         assertNotNull(component);
         assertNotNull(groupComponent);
     }
 
-    @DisplayName("Проверка поиска предмета по Id")
+    @DisplayName("2. Проверка поиска предмета по Id.")
     @SqlTest
     void findById() {
         Subject subject = component.findById(1L).get();
@@ -48,20 +55,20 @@ class SubjectComponentImplTest {
                 .contains("the same pedagogy as 6.0002 (Introduction to Computational Thinking"));
     }
 
-    @DisplayName("Проверка поиска предмета по Id если такого предмета нет выкидывается исключение")
+    @DisplayName("3. Проверка поиска предмета по Id если такого предмета нет выкидывается исключение.")
     @SqlTest
     void findByIdOrDie() {
         assertThrows(SubjectNotFoundException.class, ()-> component.findByIdOrDie(140L));
     }
 
 
-    @DisplayName("Проверка поиска всех предметов")
+    @DisplayName("4. Проверка поиска всех предметов.")
     @SqlTest
     void findAll() {
         assertEquals(7, component.findAll().size());
     }
 
-    @DisplayName("Проверка поиска предмета пол имени")
+    @DisplayName("5. Проверка поиска предмета пол имени.")
     @SqlTest
     void getSubjectsByName() throws Exception {
         List<Subject> subjects = component.getSubjectsByName("Algebra");
@@ -70,5 +77,53 @@ class SubjectComponentImplTest {
         assertEquals("Linear Algebra and Optimization", subject.getName());
         assertTrue(subject.getDescription()
                 .contains(" same fundamental concepts as 18.06 with a view toward modeling,"));
+    }
+
+    @DisplayName("6. Проверка поиска предмета пол имени.")
+    @Test
+    @SqlGroup({
+            @Sql(
+                    scripts = "/db/sql/clean.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insert.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insertSubject.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+            @Sql(
+                    scripts = "/db/sql/insertGS.sql ",
+                    executionPhase = BEFORE_TEST_METHOD,
+                    config = @SqlConfig(transactionMode = ISOLATED)),
+    })
+    void updateSubjectById() throws ParseException {
+        Subject subject = component.findById(1L).get();
+        assertNotNull(subject);
+        assertEquals("Introduction to Computational Science and Engineering", subject.getName());
+        assertTrue(subject.getDescription()
+                .contains("the same pedagogy as 6.0002 (Introduction to Computational Thinking"));
+
+        subject.setName("testName");
+        subject.setDescription("testtDescription");
+        component.updateSubjectById(1L, subject);
+        Subject subjectNew = component.findById(1L).get();
+        assertNotNull(subjectNew);
+        assertEquals("testName", subjectNew.getName());
+        assertEquals("testtDescription", subjectNew.getDescription());
+    }
+
+    @DisplayName("7. Проверка удаления предмета по Id.")
+    @SqlTest
+    void deleteSubjectById() throws Exception {
+        int count = component.findAll().size();
+        Subject subject = component.findByIdOrDie(1L);
+        assertNotNull(subject);
+        component.deleteSubjectById(1L);
+
+        Optional<Subject> subjectNew = component.findById(1L);
+        assertTrue(subjectNew.isEmpty());
     }
 }
