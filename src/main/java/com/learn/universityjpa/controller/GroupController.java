4bc40143bc3a7449501c964b1d23367d0874689f
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 13:33:27 +0500

    update class GroupController change methods: getSubjectsByGroupId, createGroup, addSubject, deleteSubject. Add methods checkSubject, builder

--- src/main/java/com/learn/universityjpa/controller/GroupController.java
+++ src/main/java/com/learn/universityjpa/controller/GroupController.java
@@ -7,10 +7,10 @@ import com.learn.universityjpa.controller.model.response.SubjectResponse;
 import com.learn.universityjpa.entity.Group;
 import com.learn.universityjpa.repo.GroupComponent;
 import com.learn.universityjpa.repo.StudentComponent;
+import com.learn.universityjpa.repo.SubjectComponent;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,9 +19,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -30,6 +29,7 @@ import java.util.stream.Collectors;
  * class GroupController
  * для работы с web сайтом /groups
  */
+
 @RestController
 @Validated
 @Tag(name = "API работы с группами")
@@ -38,10 +38,12 @@ import java.util.stream.Collectors;
 public class GroupController {
     private final GroupComponent groupComponent;
     private final StudentComponent studentComponent;
+    private final SubjectComponent subjectComponent;
+
     @GetMapping()
     public List<GroupResponse> getAllGroups() {
         return groupComponent.findAll().stream().map(
-                (x)-> new GroupResponse(x)
+                (x) -> new GroupResponse(x)
         ).collect(Collectors.toList());
     }
 
@@ -56,10 +58,26 @@ public class GroupController {
     public List<SubjectResponse> getSubjectsByGroupId(
             @PathVariable(name = "id") final long id
     ) throws Exception {
-        return groupComponent.findByIdOrDie(id).getSubjects().stream().map(
-                (x)-> new SubjectResponse(x)
-        ).collect(Collectors.toList());
+        return groupComponent
+                .findByIdOrDie(id)
+                .getSubjects()
+                .stream()
+                .map((x)-> new SubjectResponse(x))
+                .collect(Collectors.toList());
     }
+
+    @GetMapping("/{id}/checksubject")
+    public boolean checkSubject(@RequestBody final String name,
+            @PathVariable(name = "id") final long id
+    ) throws Exception {
+        return groupComponent
+                .findByIdOrDie(id)
+                .getSubjects()
+                .stream()
+                .filter((x)->x.getName().equals(name))
+                .count() > 0;
+    }
+
     @GetMapping("/{id}/students")
     public List<StudentResponse> getStudentsByGroupId(
             @PathVariable(name = "id") final long id
@@ -69,10 +87,8 @@ public class GroupController {
         ).collect(Collectors.toList());
     }
     @PostMapping()
-    public Group  createGroup(@RequestBody GroupRequest request) {
-        Group group = request.groupBuilder();
-        groupComponent.commit(group);
-        return group;
+    public GroupResponse createGroup(@RequestBody GroupRequest request) {
+        return  new GroupResponse(groupComponent.commit(builder(request)));
     }
 
     @DeleteMapping("/{id}")
@@ -86,28 +102,32 @@ public class GroupController {
     public void updateGroup(@RequestBody GroupRequest request,
             @PathVariable(name = "id") final long id
     ) throws Exception {
-        groupComponent.updateGroupById(id, request.groupBuilder());
+        groupComponent.updateGroupById(id, builder(request));
+    }
+
+    @PutMapping("/{id}/addSubject/{idSubject}")
+    public void addSubject(
+            @PathVariable(name = "id") final long id,
+            @PathVariable(name = "idSubject") final long idSubject
+    ) throws Exception {
+        groupComponent.addSubject(groupComponent.findByIdOrDie(id), subjectComponent.findByIdOrDie(idSubject));
+    }
+    @PutMapping("/{id}/deleteSubject/{idSubject}")
+    public void deleteSubject(
+            @PathVariable(name = "id") final long id,
+            @PathVariable(name = "idSubject") final long idSubject
+    ) throws Exception {
+        groupComponent.deleteSubject(groupComponent.findByIdOrDie(id), subjectComponent.findByIdOrDie(idSubject));
+    }
+    public Group builder(GroupRequest request) {
+        Group group = new Group();
+        if (Optional.ofNullable(request.getId()).isPresent()) {
+            group.setId(request.getId());
+        }
+        group.setName(request.getName());
+        group.setSpecification(request.getSpecification());
+        return group;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
