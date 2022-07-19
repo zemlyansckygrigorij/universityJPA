797c5ca0d79510a0374009ff29840d0f7fe6c4d0
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-19 14:03:27 +0500

    update class UniversityJpaApplication  checkstyle

--- src/main/java/com/learn/universityjpa/UniversityJpaApplication.java
+++ src/main/java/com/learn/universityjpa/UniversityJpaApplication.java
@@ -11,9 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class UniversityJpaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UniversityJpaApplication.class, args);
     }
-
 }
