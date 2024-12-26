d4d0f1f41e5f5fcb9361bf116c81ef1e37d8ee17
zemlyansckyG <grigory.zemlyansky@ecm-consulting.ru>
2024-12-26 19:16:43 +0500

    create interface SubjectResponseRepository for work with redis

---
+++ src/main/java/com/learn/universityjpa/cache/repo/SubjectResponseRepository.java
@@ -0,0 +1,7 @@
+package com.learn.universityjpa.cache.repo;
+
+import com.learn.universityjpa.controller.model.response.SubjectResponse;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubjectResponseRepository extends CrudRepository<SubjectResponse, String> {
+}
