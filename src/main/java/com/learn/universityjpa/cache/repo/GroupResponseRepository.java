d1e7ef4ac34ee58a17b7f38322fb1b8a18631219
zemlyansckyG <grigory.zemlyansky@ecm-consulting.ru>
2024-12-27 17:47:54 +0500

    create repos

--- src/main/java/com/learn/universityjpa/cache/repo/GroupResponseRepository.java
+++ src/main/java/com/learn/universityjpa/cache/repo/GroupResponseRepository.java
@@ -2,6 +2,8 @@ package com.learn.universityjpa.cache.repo;
 
 import com.learn.universityjpa.controller.model.response.GroupResponse;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroupResponseRepository extends CrudRepository<GroupResponse, String> {
 }
--- src/main/java/com/learn/universityjpa/cache/repo/SubjectResponseRepository.java
+++ src/main/java/com/learn/universityjpa/cache/repo/SubjectResponseRepository.java
@@ -2,6 +2,8 @@ package com.learn.universityjpa.cache.repo;
 
 import com.learn.universityjpa.controller.model.response.SubjectResponse;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubjectResponseRepository extends CrudRepository<SubjectResponse, String> {
 }
