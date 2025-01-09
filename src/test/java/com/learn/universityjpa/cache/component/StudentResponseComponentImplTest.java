021a92a4ff14161a6075bd23d937ea1c4a6bb136
zemlyansckyG <grigory.zemlyansky@ecm-consulting.ru>
2025-01-09 09:38:39 +0500

    create class StudentResponseComponentImplTest

---
+++ src/test/java/com/learn/universityjpa/cache/component/StudentResponseComponentImplTest.java
@@ -0,0 +1,32 @@
+package com.learn.universityjpa.cache.component;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentResponseComponentImplTest {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByIdOrDie() {
+    }
+
+    @Test
+    void commit() {
+    }
+
+    @Test
+    void deleteStudentById() {
+    }
+
+    @Test
+    void updateStudentById() {
+    }
+}
