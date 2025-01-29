18eda16b970b82fd06e8f63cb56ae1e3d16c858b
zemlyansckyG <grigory.zemlyansky@ecm-consulting.ru>
2025-01-29 11:37:35 +0500

    create interface UserRepo

---
+++ src/main/java/com/learn/universityjpa/db/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package com.learn.universityjpa.db.repo;
+
+
+import com.learn.universityjpa.config.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByName(String username);
+}
