83e06915b20d22219f186b056ec3fe54762e3d99
zemlyansckyG <grigory.zemlyansky@ecm-consulting.ru>
2025-01-29 11:32:16 +0500

    create file insertIntoUser.sql

---
+++ src/main/resources/insertIntoUser.sql
@@ -0,0 +1,7 @@
+
+insert into user(id, name ,password,role) values
+(1,'Anthony','$2a$05$MjLgSZSY7ap9zQyTq7RlsOOU9OmFLIq.Qb8s53CPJt3yWYuvicA/O','ADMIN'),
+(2,'Austin','$2a$05$eGXRrgmggh6TFN8j.ai43uNfLb98YhtvjLvebEu9UQU..ipoPYXkG','USER'),
+(3,'Brandon','$2a$05$/gcMizxO1w2WH52qBv2VyehLscKffDSfCyFCJeei6bZU82pr18Nji','USER');
+
+
