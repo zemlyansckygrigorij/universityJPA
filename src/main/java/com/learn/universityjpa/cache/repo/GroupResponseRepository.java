7862350568b64a47eef41739700d4f5771eb5473
zemlyansckyG <zemlyanscky.grigorij@yandex.ru>
2025-02-11 17:53:12 +0500

    checkstyle

--- src/main/java/com/learn/universityjpa/cache/repo/GroupResponseRepository.java
+++ src/main/java/com/learn/universityjpa/cache/repo/GroupResponseRepository.java
@@ -4,6 +4,11 @@ import com.learn.universityjpa.controller.model.response.GroupResponse;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * repository GroupResponseRepository for work with cache repository
+ */
 @Repository
 public interface GroupResponseRepository extends CrudRepository<GroupResponse, String> {
 }
