720170b27ef8680a29726ad4bac9657f2ce8ce68
zemlyansckyG <zemlyanscky.grigorij@yandex.ru>
2025-02-11 17:58:28 +0500

    checkstyle

--- src/main/java/com/learn/universityjpa/cache/repo/SubjectResponseRepository.java
+++ src/main/java/com/learn/universityjpa/cache/repo/SubjectResponseRepository.java
@@ -4,6 +4,11 @@ import com.learn.universityjpa.controller.model.response.SubjectResponse;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * repository SubjectResponseRepository for work with cache repository
+ */
 @Repository
 public interface SubjectResponseRepository extends CrudRepository<SubjectResponse, String> {
 }
