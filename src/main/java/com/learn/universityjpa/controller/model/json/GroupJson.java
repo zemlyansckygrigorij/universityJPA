70f3f0cc3c5f1ca061f51b1d2672f56bf6ee9a0c
zemlyansckyG <zemlyanscky.grigorij@yandex.ru>
2025-02-11 17:52:21 +0500

    checkstyle

--- src/main/java/com/learn/universityjpa/controller/model/json/GroupJson.java
+++ src/main/java/com/learn/universityjpa/controller/model/json/GroupJson.java
@@ -1,11 +1,15 @@
 package com.learn.universityjpa.controller.model.json;
 
-import com.learn.universityjpa.controller.model.response.GroupResponse;
 import com.learn.universityjpa.db.entity.Group;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class GroupJson
+ */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,17 +17,10 @@ public class GroupJson {
     private Long id;
     private String name;
     private String specification;
-    public GroupJson(Group group){
+
+    public GroupJson(Group group) {
         this.id = group.getId();
         this.name = group.getName();
         this.specification = group.getSpecification();
-    }
-
-    public GroupResponse getGroupResponse(){
-        GroupResponse response = new GroupResponse();
-        response.setId(this.id);
-        response.setName(this.name);
-        response.setSpecification(this.specification);
-        return response;
     }
 }
