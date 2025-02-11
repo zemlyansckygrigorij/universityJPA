948d803311bf53f6ce22c8cfa411dac0019c9338
zemlyansckyG <zemlyanscky.grigorij@yandex.ru>
2025-02-11 18:07:06 +0500

    create class TeacherResponseComponentImplTest

---
+++ src/test/java/com/learn/universityjpa/cache/component/TeacherResponseComponentImplTest.java
@@ -0,0 +1,81 @@
+package com.learn.universityjpa.cache.component;
+
+import com.learn.universityjpa.cache.repo.TeacherResponseRepository;
+import com.learn.universityjpa.controller.model.response.TeacherResponse;
+import com.learn.universityjpa.db.component.TeacherComponent;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+class TeacherResponseComponentImplTest {
+    @Autowired
+    private TeacherComponent component;
+    @Autowired
+    private TeacherResponseRepository repo;
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByIdOrDie() {
+    }
+
+    @Test
+    void commit() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void getTeachersByName() {
+    }
+
+    @Test
+    void deleteTeacherById() {
+    }
+
+    @Test
+    void updateTeacherById() {
+    }
+
+    @Test
+    void findAllSubjects() {
+    }
+
+    @Test
+    void checkSubject() {
+    }
+
+    @Test
+    void addSubject() {
+    }
+
+    @Test
+    void deleteSubject() {
+    }
+
+    @Test
+    void testDeleteSubject() {
+    }
+
+    @Test
+    void testAddSubject() {
+    }
+    @Test
+    void insertAllSubject() {
+        repo.deleteAll();
+        component.findAll().forEach(t->{
+            repo.save(new TeacherResponse(t));
+        });
+        assertEquals(repo.count(),12);
+    }
+}
