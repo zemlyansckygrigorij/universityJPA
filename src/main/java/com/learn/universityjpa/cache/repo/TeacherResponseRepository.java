7a718acc08df662a7e284c2487e0065e72caac5c
zemlyansckyG <zemlyanscky.grigorij@yandex.ru>
2025-02-11 17:59:40 +0500

    checkstyle

--- src/main/java/com/learn/universityjpa/cache/repo/TeacherResponseRepository.java
+++ src/main/java/com/learn/universityjpa/cache/repo/TeacherResponseRepository.java
@@ -4,6 +4,11 @@ import com.learn.universityjpa.controller.model.response.TeacherResponse;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * repository TeacherResponseRepository for work with cache repository
+ */
 @Repository
 public interface TeacherResponseRepository extends CrudRepository<TeacherResponse, String> {
 }
